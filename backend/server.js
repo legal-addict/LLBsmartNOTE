f2ee3ec5790b2830a1462f607ccc1799fc1ace84
legal.addict <munazzapaloba25@gmail.com>
2026-06-21 07:34:43 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,12 @@
 const express = require("express");
+
+const app = express();
+
+app.use((req, res, next) => {
+  console.log("REQUEST:", req.method, req.url);
+  next();
+});
+const express = require("express");
 const cors = require("cors");
 const crypto = require("crypto");
 
