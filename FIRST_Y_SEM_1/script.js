77d71583b1266838be8b0585c6a098ee6276d949
legal.addict <munazzapaloba25@gmail.com>
2026-06-21 07:31:48 +0530

    Update script.js

--- FIRST_Y_SEM_1/script.js
+++ FIRST_Y_SEM_1/script.js
@@ -1,6 +1,8 @@
-console.log("SCRIPT START");
+const express = require("express");
+const cors = require("cors");
+
 app.use((req, res, next) => {
-  console.log("REQUEST:", req.method, req.url);
+  console.log(req.method, req.url);
   next();
 });
 window.buyNote = async function(noteName, price) {
